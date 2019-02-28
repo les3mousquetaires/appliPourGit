0b1e76867f127104b689e417118321fbe2ecb833
FlorianeBV <FlorianeBV@floriane.bichyvagnol@gmail.com>
2019-02-28 15:29:24 +0100

    ajout de la classe Employe.cs

---
+++ appliPourGit/Employe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appliPourGit
+{
+    class Employe
+    {
+        private string id;
+        private string nom;
+        private string prenom;
+    }
+}
